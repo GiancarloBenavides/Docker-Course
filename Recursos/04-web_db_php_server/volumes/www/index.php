84c8d356a50b915faca78f9a905856fe5a7f3944
Giancarlo <giancarlobenavides@hotmail.com>
2022-10-23 19:20:10 -0500

    ex-09

--- Recursos/04-web_db_php_server/volumes/www/index.php
+++ Recursos/04-web_db_php_server/volumes/www/index.php
@@ -10,7 +10,15 @@
 
 $author = "@gncdev";
 $version = "1.0";
+$webpage = "https://twitter.com/gncdev"
 
-echo 1+1
+echo "<h1>hola mundo</h1>";
+echo "<p>La suma es: </p>";
+echo 1+1;
+echo "<p><b>Autor:</b><a href="$webpage"> $author.</a></p>";
 
 ?>
+
+<p><b>Version:</b> <?php $version ?></p>
+<p><b>Date:</b> <?php echo date("d.m.Y");?></p>
+<p><b>Time:</b> <?php echo date("H:i:s");?></p>
